fe9e79cbc74147f116330e310ace5e3d2ab0df9f
soydachi <dachi.gogotchuri@gmail.com>
2026-04-27 03:23:25 +0200

    claudeline 0.2.0

--- Formula/claudeline.rb
+++ Formula/claudeline.rb
@@ -7,22 +7,22 @@ class Claudeline < Formula
   on_macos do
     on_arm do
       url "https://github.com/arcasilesgroup/claudeline/releases/download/v#{version}/claudeline-darwin-arm64"
-      sha256 "REPLACE_AT_RELEASE_TIME"
+      sha256 "181129bd1c7d8a4b0034505498a93142b22455998a2df48d7695a07dc3adc1d8"
     end
     on_intel do
       url "https://github.com/arcasilesgroup/claudeline/releases/download/v#{version}/claudeline-darwin-x64"
-      sha256 "REPLACE_AT_RELEASE_TIME"
+      sha256 "3e932e22cd5820c79f5f9277c75c7052e8b4f8bb890401f0d8429f9a76a2ea66"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/arcasilesgroup/claudeline/releases/download/v#{version}/claudeline-linux-x64"
-      sha256 "REPLACE_AT_RELEASE_TIME"
+      sha256 "0b9095d1325e11e4271c56c09f00380fcf5f2ccf9512561fcf9fac2eaf113539"
     end
     on_arm do
       url "https://github.com/arcasilesgroup/claudeline/releases/download/v#{version}/claudeline-linux-arm64"
-      sha256 "REPLACE_AT_RELEASE_TIME"
+      sha256 "a246178ef4faa0afe6dc0ec1e3221cefa4730c79595364fa2c55dfa773a2e923"
     end
   end
 
